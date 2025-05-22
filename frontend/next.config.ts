bbefd97beeb657037629e31f60a8f8679df8cbe3
samuelthen <samuel.then@gmail.com>
2025-05-23 02:11:04 +0800

    Update config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// frontend/next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
